ab5b64b2a32991ea85d35694ec1ae28de9a7d79a
srnnnn <tpfla311@naver.com>
2024-06-03 18:40:34 +0900

    mission1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import './App.css'
-import MovieCard from './pages/MovieCard'
-import movieListData from '../movieListData.json';
-import { Route, Routes } from 'react-router-dom';
-import MovieDetail from './pages/MovieDetail';
+import "./App.css";
+import MovieCard from "./pages/MovieCard";
+import movieListData from "../movieListData.json";
+import { Route, Routes } from "react-router-dom";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
-
-  console.log(movieListData.results)
+  console.log(movieListData.results);
   return (
     <>
       <Routes>
-        <Route path='/' element={<MovieList/>}/>
-        <Route path='/details' element={<MovieDetail/>}/>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/details" element={<MovieDetail />} />
       </Routes>
     </>
-  )
+  );
 }
 
 function MovieList() {
-  return(
-    <div className='container'>
+  return (
+    <div className="container">
       {movieListData.results.map((movie) => (
-        <MovieCard 
-          key={movie.id} 
-          poster_path={movie.poster_path} 
-          title={movie.title} 
-          vote_avg={movie.vote_average}/>
-      )) }
+        <MovieCard
+          key={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+          vote_avg={movie.vote_average}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/MovieCard.jsx
+++ src/pages/MovieCard.jsx
@@ -1,22 +1,27 @@
-import './MovieCard.css'
-import { Link } from 'react-router-dom';
+import "./MovieCard.css";
+import { Link } from "react-router-dom";
 
-const MovieCard = ({poster_path, title, id, vote_avg}) => {
+const MovieCard = ({ poster_path, title, id, vote_avg }) => {
   return (
     <>
-    <div className='movieCard'>
-    <Link to={'/details'}>
-      <img className='movieImg'
-          src={`https://image.tmdb.org/t/p/original${poster_path}`}
-          alt={`${title} 포스터 사진`}
+      <div className="movieCard" key={id}>
+        <Link
+          to={"/details"}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <img
+            className="movieImg"
+            src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+            alt={`${title} 포스터 사진`}
           />
-      </Link>
-      <div className='movieTitle'><stron>{title}</stron></div>
-      <div className='movieVote'>평점 {vote_avg.toFixed(1)}</div>
-    </div>
+          <div className="movieTitle">
+            <strong>{title}</strong>
+          </div>
+        </Link>
+        <div className="movieVote">평점 {vote_avg.toFixed(1)}</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MovieCard
-
+export default MovieCard;
